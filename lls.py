13296905d29f6b05ad728349312f4a95831ab9cc
farhad <farhadkh736@gmail.com>
2025-07-11 23:16:28 +0330

    Create lls.py

---
+++ lls.py
@@ -0,0 +1,25 @@
+import numpy as np
+from numpy.linalg import inv
+
+
+
+
+# w = شیب خط
+# w = (X.T * X) ** -1 * X.T * Y
+# w = np.matmul(np.matmul(np.linalg.inv(np.matmul(X_train.T, X_train)), X_train.T), Y_train)
+class LLS:
+    def __init__(self):
+        self.w = None
+    
+    def fit(self, X_train, Y_train):
+        #self.w = np.matmul(np.matmul(np.linalg.inv(np.matmul(X_train.T, X_train)), X_train.T), Y_train)
+        self.w = inv(X_train.T @ X_train) @ X_train.T @ Y_train
+        return self.w
+        
+    def predict(self, X_test):
+        Y_pred = X_test @ self.w
+        return Y_pred
+    
+    def eval
+    
+    
