7791c90dfc95d7b4e8bc3376d0fb540ab3a1065c
farhad <farhadkh736@gmail.com>
2025-07-15 22:28:02 +0330

    Update lls.py

--- lls.py
+++ lls.py
@@ -18,7 +18,14 @@ class LLS:
         Y_pred = X_test @ self.w
         return Y_pred
     
-    def evaluate(self, X_test, Y_test):
-        pass
+    def evaluate(self, X_test, Y_test, metric):
+        Y_pred = self.predict(X_test)
+        
+        if metric == 'mae':
+            loss = np.sum(np.abs(Y_test - Y_pred)) / len(Y_test)
+        elif metric == 'mse':
+            loss = np.sum((Y_test - Y_pred) ** 2) / len(Y_test) 
+            
+        return loss
     
     
